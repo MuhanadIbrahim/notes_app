b75d59ee00461ce471ed44adf30fa43149405024
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-09-18 20:18:11 +0300

    make custom note view body

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_app_bar.dart';
+
+import '../widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -7,17 +8,7 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            CustomAppBar(),
-          ],
-        ),
-      ),
+      body:NotesviewBody()
     );
   }
 }
---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class NotesviewBody extends StatelessWidget {
+  const NotesviewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            CustomAppBar(),
+          ],
+        ),
+      );
+  }
+}
