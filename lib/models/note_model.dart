86b719baa399a6bd96d3687e613f39c9041fd763
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-09-23 19:19:40 +0300

    create note Model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+
+class NoteModel {
+ 
+  final String title;
+  final String subtitle;
+  final int color;
+  final String date;
+
+  NoteModel(this.title, this.subtitle, this.color, this.date);
+ 
+}
